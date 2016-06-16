812043462dff6e600f2a7abf05df20a7053777ba
Ehsan619 <ehsan.hesam13@gmail.com>
2016-06-16 16:54:10 +0430

    Handlers/Channles : Implement all request handlers of Helps
    getNearestDc, getAppUpdate, saveAppLog, getInviteText, getSupport
    getAppChangelog, getTermsOfService

--- TeleTurk.Core/Handlers/HelpHandlers.cs
+++ TeleTurk.Core/Handlers/HelpHandlers.cs
@@ -12,6 +12,92 @@ namespace TeleTurk.Core.Handlers
 {
     class HelpHandlers
     {
-        
+        private MtProtoSender _sender;
+
+        public async Task<TL.ConfigType> getConfig()
+        {
+            var request = new TL.HelpGetConfigRequest();
+
+            await _sender.Send(request);
+            await _sender.Receive(request);
+
+            return (TL.ConfigType)request.Result;
+        }
+
+        public async Task<TL.NearestDcType> getNearestDc()
+        {
+            var request = new TL.HelpGetNearestDcRequest();
+
+            await _sender.Send(request);
+            await _sender.Receive(request);
+
+            return (TL.NearestDcType)request.Result;
+        }
+
+        public async Task<HelpAppUpdate> getAppUpdate(string device_model, string system_version, string app_version, string lang_code)
+        {
+            var request = new TL.HelpGetAppUpdateRequest(device_model, system_version, app_version, lang_code);
+
+            await _sender.Send(request);
+            await _sender.Receive(request);
+
+            return request.Result;
+        }
+
+        public async Task<bool> saveAppLog(List<InputAppEvent> events)
+        {
+            var request = new TL.HelpSaveAppLogRequest(events);
+
+            await _sender.Send(request);
+            await _sender.Receive(request);
+
+            return request.Result is TL.BoolTrueType ? true : false;
+        }
+
+        public async Task<string> getInviteText(string lang_code)
+        {
+            var request = new TL.HelpGetInviteTextRequest(lang_code);
+
+            await _sender.Send(request);
+            await _sender.Receive(request);
+
+            var element = (TL.HelpInviteTextType)(request.Result);
+
+            return element.Message;
+        }
+
+        public async Task<Tuple<string , User>> getSupport()
+        {
+            var request = new TL.HelpGetSupportRequest();
+
+            await _sender.Send(request);
+            await _sender.Receive(request);
+
+            var elemnt = (TL.HelpSupportType)request.Result;
+
+            return new Tuple<string, User>(elemnt.PhoneNumber, elemnt.User);
+        }
+
+        public async Task<HelpAppChangelog> getAppChangelog(string device_model, string system_version, string app_version, string lang_code)
+        {
+            var request = new TL.HelpGetAppChangelogRequest(device_model, system_version, app_version, lang_code);
+
+            await _sender.Send(request);
+            await _sender.Receive(request);
+
+            return request.Result;
+        }
+
+        public async Task<string> getTermsOfService()
+        {
+            var request = new TL.HelpGetTermsOfServiceRequest();
+
+            await _sender.Send(request);
+            await _sender.Receive(request);
+
+            var elemnt = (TL.HelpTermsOfServiceType)request.Result;
+
+            return elemnt.Text;
+        }
     }
 }
