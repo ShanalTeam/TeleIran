7f910633d9eece9621e2028de15f69e4597523db
Ehsan619 <ehsan.hesam13@gmail.com>
2016-06-16 16:46:34 +0430

    Handlers : Add place holder for request handlers

---
+++ TeleTurk.Core/Handlers/ChannelHandlers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeleTurk.Core.Auth;
+using TeleTurk.Core.MTProto;
+using TeleTurk.Core.MTProto.Crypto;
+using TeleTurk.Core.Network;
+
+namespace TeleTurk.Core.Handlers
+{
+    class ChannelHandlers
+    {
+        
+    }
+}
---
+++ TeleTurk.Core/Handlers/PhotosHandlers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeleTurk.Core.Auth;
+using TeleTurk.Core.MTProto;
+using TeleTurk.Core.MTProto.Crypto;
+using TeleTurk.Core.Network;
+
+namespace TeleTurk.Core.Handlers
+{
+    class PhotosHandlers
+    {
+    }
+}
